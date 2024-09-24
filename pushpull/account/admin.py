9f080343f95c4741c978f533a22400955c92a30e
prestij1987 <leek.style@mail.ru>
2024-09-24 20:18:50 +0300

    add admin and models

--- pushpull/account/admin.py
+++ pushpull/account/admin.py
@@ -1,3 +1,12 @@
-from django.contrib import admin
+
 
 # Register your models here.
+
+from django.contrib import admin 
+from .models import Task 
+  
+# Register your models here. 
+ 
+@admin.register(Task) 
+class TaskAdmin(admin.ModelAdmin): 
+    list_display = [field.name for field in Task._meta.get_fields()]
