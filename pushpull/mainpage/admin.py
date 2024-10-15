136027835b9b115609a4401e171f5233389a2cee
prestij1987 <leek.style@mail.ru>
2024-10-15 19:14:33 +0300

    add admin models

--- pushpull/mainpage/admin.py
+++ pushpull/mainpage/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+
+
+from django.contrib import admin 
+from .models import Dist
+  
+# Register your models here. 
+ 
+@admin.register(Dist) 
+class TaskAdmin(admin.ModelAdmin): 
+    list_display = [field.name for field in Dist._meta.get_fields()]
